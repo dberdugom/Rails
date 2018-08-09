f81d13c3a0297371679b355c5e0ddbb3c74db792
dberdugom <dberdugom@gmail.com>
2018-08-09 13:58:50 -0500

    primer commit

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,53 @@
+class ImagesController < ApplicationController
+
+    before_action :set_image, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @images = Image.all            # variable de tipo array @images
+   end
+    
+    def show   
+         
+    end
+
+    def new 
+        @image = Image.new
+    end
+
+    def edit       
+    end
+ 
+    def create
+
+      #render plain: params[:image].inspect
+      @image = Image.new images_params
+      @image.save
+
+      #redirect_to @image  #redirecciona a una nueva vista
+
+    end
+
+    def update
+        @image = Image.find params[:id] # buscamos nuestro id del objeto imagen
+        @image.update images_params 
+        redirect_to @image
+    end
+
+    def destroy
+        @image = Image.find params[:id]
+        @image.destroy
+        redirect_to images_path 
+    end
+
+    private
+
+    def images_params
+        params.require(:image).permit(:description)
+    end 
+    
+    def set_images
+        @image = Image.find params[:id]
+    end 
+    
+
+end
